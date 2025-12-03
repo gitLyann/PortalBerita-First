47322574d2def90eaebd0df73a6afb7126c343fa
gitLyann <ladrazmamora05@gmail.com>
2025-12-03 09:57:55 +0700

    initial commit

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form //tambah edit data
+    {
+        return $form
+            ->schema([
+                //
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    // ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                    // ->live(debounce: 250)
+                    ->maxLength(255),
+
+                // Forms\Components\TextInput::make('slug')
+                //     ->required()
+                //     ->disabled(),
+
+                Forms\Components\FileUpload::make('icon')
+                    ->image()
+                    ->required(),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table //tampil data setelah ditambah
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\ImageColumn::make('icon'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array //memiliki realtionship ke table lain
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
